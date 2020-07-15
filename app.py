5301aada9ffdedfc02f4e8adaaac6be246e22890
Tristan Brooks <3stanbrooks@gmail.com>
2020-07-14 19:32:02 -0500

    Added flask_restx

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_restx import Api, Resource, fields
+
 app = Flask(__name__)
+api = Api(app, version='1.0', title='barax', description='REST API for our guild management application.')
 
 @app.route('/')
 def hello_world():
